f07a76e91b642afc06224b1dbd9a0bc5dddfcadf
Symeonidis Klimis <symeonidis@intralot.com>
2017-10-23 11:59:10 +0300

    rm logrotate

--- build/build.sh
+++ build/build.sh
@@ -11,4 +11,6 @@ docker build  -t mylamp/logrotate -f mysql/Dockerfile .
 
 docker build  -t mylamp/redis -f redis/Dockerfile .
 
-docker build --build-arg LOCAL_USER_ID=`id -u $USER` --no-cache=true  -t mylamp/web -f web/Dockerfile .
+docker build --build-arg LOCAL_USER_ID=`id -u $USER` -t mylamp/web -f web/Dockerfile .
+
+docker build  --no-cache=true -t mylamp/phpmyadmin -f phpmyadmin/Dockerfile .
--- run/conf/phpmyadmin/config.user.inc.php
+++ run/conf/phpmyadmin/config.user.inc.php
@@ -78,4 +78,3 @@ $cfg['Servers'][$i]['AllowNoPassword']     = TRUE;
 $i++;
 
 
-
